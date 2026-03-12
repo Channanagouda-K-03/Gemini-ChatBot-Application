664aa80a1ee7c12d034c3d177e8c64361a3210c6
Channanagowda K <159411835+Channanagouda-K-03@users.noreply.github.com>
2026-03-12 20:55:59 +0530

    Update AIController.java

--- src/main/java/com/ai/gemini_chat/Controller/AIController.java
+++ src/main/java/com/ai/gemini_chat/Controller/AIController.java
@@ -3,16 +3,14 @@ package com.ai.gemini_chat.Controller;
 import com.ai.gemini_chat.Service.QnAService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/qna")
+@CrossOrigin(origins = "*")
 public class AIController {
 
     private final QnAService qnAService;
